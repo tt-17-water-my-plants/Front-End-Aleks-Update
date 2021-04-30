7ac86b17b2cf2644cf98ebf72d2d7a7b25ad9be8
devin mitchell <devin.a.mitchell@gmail.com>
2021-04-29 22:35:36 -0600

    added icon instead of photo

--- water-my-plants/src/components/Styles/HomeStyles.js
+++ water-my-plants/src/components/Styles/HomeStyles.js
@@ -7,9 +7,23 @@ export const Container = styled.div`
     justify-content: center;
     height: 50vh;
     
+    .icon {
+        background: #583E23;
+        border-radius: 50%;
+        padding: 0 3%;
+        
+        box-shadow: 5px 5px 2px 1px rgba(0, 0, 0, 0.2); 
+
+        &:first-child {
+            font-size: 12rem;
+            color: #E3DCC2;
+        }
+    }
+    
+
     button {
         font-size: 2rem;
-        margin: 20px;
+        margin: 20px 0 0 ;
         border-radius: 20px;
         padding: 5px 15px;
         background-color: #E3DC95;
